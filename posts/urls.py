dcc6581850b30aa7df35ab9e56963c2b06d33a41
Georgi Horozov | GOPET TRANS <georgi.horozov@gopettrans.com>
2026-03-30 16:31:26 +0300

    Fix PUT logic and DRF views

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("homepage/", views.homepage, name="page_home"),
+    path("listcreate/", views.PostListCreateView.as_view(), name="list_create"),
+    path("<int:pk>/", views.PostRetrieveUpdateDeleteView.as_view(), name="post_detail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,47 @@
-from rest_framework import status
+from rest_framework import generics, mixins, status
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
+
+from posts.serializers import PostSerializer
+
+from .models import Post
 
 
 @api_view(http_method_names=["GET", "POST"])
 def homepage(request:Request):
     response={"message": "Hello World!"}
     return Response(data=response, status=status.HTTP_200_OK)
+
+class PostListCreateView(
+    generics.GenericAPIView, 
+    mixins.ListModelMixin, 
+    mixins.CreateModelMixin):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request:Request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request:Request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+
+class PostRetrieveUpdateDeleteView(
+    generics.GenericAPIView,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin):
+
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    
+    def get(self, request:Request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+    
+    def put(self, request:Request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+    
+    def delete(self, request:Request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
